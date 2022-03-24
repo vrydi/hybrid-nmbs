8e076f5193f00bab290d53a7239bbeec3fd3dea3
vrydi <57484019+vrydi@users.noreply.github.com>
2022-03-24 23:39:10 +0100

    homePage done

--- data/styles.js
+++ data/styles.js
@@ -1,11 +1,12 @@
 import tw from "twrnc";
 
 export const nmbsBlue = "#006ab3";
-export const nmbsBlueDark = "#01436E";
+export const nmbsBlueDark = "#035C97";
 export const fullContainer = tw`h-full bg-[${nmbsBlue}]`;
 export const image = { width: "100%", height: "100%", resizeMode: "contain" };
-export const title = tw`text-white text-4xl font-bold text-center`;
-export const inputContainer = tw`h-15 mx-5 rounded-xl border-b`;
-export const input = tw`text-white px-5`;
-export const inputLabel = tw`text-white text-xl`;
+export const title = tw`text-white text-4xl font-bold text-center mt-10`;
 export const inputDropdownListContainer = tw`w-8/9 mx-auto my-2`;
+export const flexBox = tw`flex flex-row items-center justify-between w-8/9 mx-auto`;
+export const button = tw`px-5 text-white py-3 bg-[${nmbsBlueDark}] rounded-lg`;
+export const buttonText = tw`text-white text-center`;
+export const regular = tw`text-white`;
--- screens/HomePage.js
+++ screens/HomePage.js
@@ -2,22 +2,20 @@ import {
   View,
   Text,
   Image,
-  TextInput,
-  FlatList,
   Keyboard,
   Pressable,
   TouchableOpacity,
+  SafeAreaView,
 } from "react-native";
 import {
+  button,
+  buttonText,
+  flexBox,
   fullContainer,
   image,
-  input,
-  inputContainer,
-  inputDropdownList,
   inputDropdownListContainer,
-  inputLabel,
+  regular,
   title,
-  TouchableWithoutFeedback,
 } from "../data/styles";
 import tw from "twrnc";
 import { useStationsContext } from "../contexts/StationContext";
@@ -39,11 +37,14 @@ export function HomePage() {
       departure: "",
       arrival: "",
       date: new Date(),
+      time: new Date(),
+      timePoint: "departure",
     },
   });
   const onSubmit = (data) => console.log(data);
 
   const [openDatePicker, setOpenDatePicker] = useState(false);
+  const [openTimePicker, setOpenTimePicker] = useState(false);
   const items = stationStringList.map((station) => ({
     label: station,
     value: station,
@@ -52,117 +53,194 @@ export function HomePage() {
   const [focusedField, setFocusField] = useState("");
 
   return (
-    <Pressable
-      onPress={() => {
-        Keyboard.dismiss();
-        setFocusField("");
-      }}
-    >
-      <View style={fullContainer}>
-        {focusedField === "" && (
-          <View style={tw`w-full max-h-60`}>
-            <Image
-              style={image}
-              source={{
-                uri: "https://pbs.twimg.com/media/Ea9cHfSWAAA6mQQ?format=jpg&name=medium",
+    <SafeAreaView>
+      <Pressable
+        onPress={() => {
+          Keyboard.dismiss();
+          setFocusField("");
+        }}
+      >
+        <View style={fullContainer}>
+          {(focusedField === "" || focusedField === "timePoint") && (
+            <View style={tw`w-full max-h-60`}>
+              <Image
+                style={image}
+                source={{
+                  uri: "https://pbs.twimg.com/media/Ea9cHfSWAAA6mQQ?format=jpg&name=medium",
+                }}
+              />
+            </View>
+          )}
+          <Text style={title}>Waar naartoe?</Text>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <>
+                <DropDownPicker
+                  containerStyle={inputDropdownListContainer}
+                  open={focusedField === "departure"}
+                  value={value}
+                  onClose={() => {
+                    onBlur;
+                    setFocusField("");
+                  }}
+                  searchable={true}
+                  setValue={onChange}
+                  onChangeValue={onChange}
+                  setOpen={() => setFocusField("departure")}
+                  closeAfterSelecting={true}
+                  items={items}
+                  maxHeight={400}
+                  searchPlaceholder="Zoek en station..."
+                  placeholder="Selecteer een vertrekstation"
+                  zIndex={3000}
+                  zIndexInverse={1000}
+                  dropDownDirection={"BOTTOM"}
+                />
+              </>
+            )}
+            name="departure"
+          />
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <>
+                <DropDownPicker
+                  // style={inputDropdownListContainer}
+                  containerStyle={inputDropdownListContainer}
+                  open={focusedField === "arrival"}
+                  value={value}
+                  onClose={() => {
+                    onBlur;
+                    setFocusField("");
+                  }}
+                  searchable={true}
+                  setValue={onChange}
+                  onChangeValue={onChange}
+                  setOpen={() => setFocusField("arrival")}
+                  closeAfterSelecting={true}
+                  items={items}
+                  maxHeight={400}
+                  searchPlaceholder="Zoek en station..."
+                  placeholder="Selecteer een aankomststation"
+                  zIndex={2000}
+                  zIndexInverse={2000}
+                  dropDownDirection={"BOTTOM"}
+                />
+              </>
+            )}
+            name="arrival"
+          />
+          <View style={flexBox}>
+            <Controller
+              control={control}
+              rules={{
+                required: true,
               }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <>
+                  <DropDownPicker
+                    // style={inputDropdownListContainer}
+                    containerStyle={tw`w-3/8`}
+                    open={focusedField === "timePoint"}
+                    value={value}
+                    onClose={() => {
+                      onBlur;
+                      setFocusField("");
+                    }}
+                    setValue={onChange}
+                    onChangeValue={onChange}
+                    setOpen={() => setFocusField("timePoint")}
+                    closeAfterSelecting={true}
+                    items={[
+                      { label: "Vertrek", value: "departure" },
+                      { label: "Aankomst", value: "arrival" },
+                    ]}
+                    maxHeight={200}
+                    placeholder="Selecteer een aankomststation"
+                    zIndex={1000}
+                    zIndexInverse={3000}
+                    dropDownDirection={"BOTTOM"}
+                  />
+                </>
+              )}
+              name="timePoint"
+            />
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TouchableOpacity
+                  onPress={() => setOpenDatePicker(true)}
+                  style={button}
+                >
+                  <Text style={buttonText}>{format(value, "dd-MM-yyyy")}</Text>
+                  {openDatePicker && (
+                    <RNDateTimePicker
+                      onChange={(res) => {
+                        setOpenDatePicker(false);
+                        onChange(new Date(res.nativeEvent.timestamp));
+                      }}
+                      value={value}
+                      minimumDate={new Date()}
+                      mode={"date"}
+                    />
+                  )}
+                </TouchableOpacity>
+              )}
+              name="date"
+            />
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TouchableOpacity
+                  onPress={() => setOpenTimePicker(true)}
+                  style={button}
+                >
+                  <Text style={buttonText}>
+                    {value.toLocaleTimeString().slice(0, 5)}
+                  </Text>
+                  {openTimePicker && (
+                    <RNDateTimePicker
+                      onChange={(res) => {
+                        setOpenTimePicker(false);
+                        onChange(new Date(res.nativeEvent.timestamp));
+                      }}
+                      value={value}
+                      minimumDate={new Date()}
+                      mode={"time"}
+                      display={"spinner"}
+                      is24Hour={true}
+                    />
+                  )}
+                </TouchableOpacity>
+              )}
+              name="time"
             />
           </View>
-        )}
-        <Text style={title}>Waar naartoe?</Text>
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <>
-              <DropDownPicker
-                containerStyle={inputDropdownListContainer}
-                open={focusedField === "departure"}
-                value={value}
-                onClose={() => {
-                  onBlur;
-                  setFocusField("");
-                }}
-                searchable={true}
-                setValue={onChange}
-                onChangeValue={onChange}
-                setOpen={() => setFocusField("departure")}
-                closeAfterSelecting={true}
-                items={items}
-                maxHeight={400}
-                searchPlaceholder="Zoek en station..."
-                placeholder="Selecteer een vertrekstation"
-                zIndex={3000}
-                zIndexInverse={1000}
-                dropDownDirection={"BOTTOM"}
-              />
-            </>
-          )}
-          name="departure"
-        />
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <>
-              <DropDownPicker
-                // style={inputDropdownListContainer}
-                containerStyle={inputDropdownListContainer}
-                open={focusedField === "arrival"}
-                value={value}
-                onClose={() => {
-                  onBlur;
-                  setFocusField("");
-                }}
-                searchable={true}
-                setValue={onChange}
-                onChangeValue={onChange}
-                setOpen={() => setFocusField("arrival")}
-                closeAfterSelecting={true}
-                items={items}
-                maxHeight={400}
-                searchPlaceholder="Zoek en station..."
-                placeholder="Selecteer een aankomststation"
-                zIndex={2000}
-                zIndexInverse={2000}
-                dropDownDirection={"BOTTOM"}
-              />
-            </>
-          )}
-          name="arrival"
-        />
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <Button onPress={() => setOpenDatePicker(true)}>
-              <Text>Date: {format(value, "dd-MM-yyyy")}</Text>
-              {openDatePicker && (
-                <RNDateTimePicker
-                  onChange={(res) => {
-                    setOpenDatePicker(false);
-                    onChange(new Date(res.nativeEvent.timestamp));
-                  }}
-                  value={value}
-                  minimumDate={new Date()}
-                  mode={"date"}
-                />
-              )}
-            </Button>
-          )}
-          name="date"
-        />
 
-        <TouchableOpacity title="submit" onPress={handleSubmit(onSubmit)}>
-          <Text>Vind me een trein</Text>
-        </TouchableOpacity>
-      </View>
-    </Pressable>
+          <View style={tw`w-1/2 mx-auto my-5`}>
+            <TouchableOpacity
+              title="submit"
+              onPress={handleSubmit(onSubmit)}
+              style={button}
+            >
+              <Text style={buttonText}>Vind me een trein</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Pressable>
+    </SafeAreaView>
   );
 }
