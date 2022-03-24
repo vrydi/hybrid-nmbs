ec133c773c0992dd97bbf5d3f259966bcac9b84f
vrydi <57484019+vrydi@users.noreply.github.com>
2022-03-24 21:31:49 +0100

    select stations with autocomplete

--- data/styles.js
+++ data/styles.js
@@ -5,3 +5,7 @@ export const nmbsBlueDark = "#01436E";
 export const fullContainer = tw`h-full bg-[${nmbsBlue}]`;
 export const image = { width: "100%", height: "100%", resizeMode: "contain" };
 export const title = tw`text-white text-4xl font-bold text-center`;
+export const inputContainer = tw`h-15 mx-5 rounded-xl border-b`;
+export const input = tw`text-white px-5`;
+export const inputLabel = tw`text-white text-xl`;
+export const inputDropdownListContainer = tw`w-8/9 mx-auto my-2`;
--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,21 +1,133 @@
-import { View, Text, Image } from "react-native";
-import { fullContainer, image, title } from "../data/styles";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  FlatList,
+  Keyboard,
+  Pressable,
+} from "react-native";
+import {
+  fullContainer,
+  image,
+  input,
+  inputContainer,
+  inputDropdownList,
+  inputDropdownListContainer,
+  inputLabel,
+  title,
+  TouchableWithoutFeedback,
+} from "../data/styles";
 import tw from "twrnc";
 import { useStationsContext } from "../contexts/StationContext";
+import { useForm, Controller } from "react-hook-form";
+import { useState } from "react";
+import DropDownPicker from "react-native-dropdown-picker";
 
 export function HomePage() {
   const { stationStringList } = useStationsContext();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+  const onSubmit = (data) => console.log(data);
+
+  const items = stationStringList.map((station) => ({
+    label: station,
+    value: station,
+  }));
+
+  const [focusedField, setFocusField] = useState("");
+
   return (
-    <View style={fullContainer}>
-      <View style={tw`w-full max-h-60`}>
-        <Image
-          style={image}
-          source={{
-            uri: "https://pbs.twimg.com/media/Ea9cHfSWAAA6mQQ?format=jpg&name=medium",
+    <Pressable
+      onPress={() => {
+        Keyboard.dismiss();
+        setFocusField("");
+      }}
+    >
+      <View style={fullContainer}>
+        {focusedField === "" && (
+          <View style={tw`w-full max-h-60`}>
+            <Image
+              style={image}
+              source={{
+                uri: "https://pbs.twimg.com/media/Ea9cHfSWAAA6mQQ?format=jpg&name=medium",
+              }}
+            />
+          </View>
+        )}
+        <Text style={title}>Waar naartoe?</Text>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
           }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <>
+              <DropDownPicker
+                containerStyle={inputDropdownListContainer}
+                open={focusedField === "departure"}
+                value={value}
+                onClose={() => {
+                  onBlur;
+                  setFocusField("");
+                }}
+                searchable={true}
+                setValue={onChange}
+                setOpen={() => setFocusField("departure")}
+                closeAfterSelecting={true}
+                items={items}
+                maxHeight={400}
+                searchPlaceholder="Zoek en station..."
+                placeholder="Selecteer een vertrekstation"
+                zIndex={3000}
+                zIndexInverse={1000}
+                dropDownDirection={"BOTTOM"}
+              />
+            </>
+          )}
+          name="departure"
+        />
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <>
+              <DropDownPicker
+                // style={inputDropdownListContainer}
+                containerStyle={inputDropdownListContainer}
+                open={focusedField === "arrival"}
+                value={value}
+                onClose={() => {
+                  onBlur;
+                  setFocusField("");
+                }}
+                searchable={true}
+                setValue={onChange}
+                setOpen={() => setFocusField("arrival")}
+                closeAfterSelecting={true}
+                items={items}
+                maxHeight={400}
+                searchPlaceholder="Zoek en station..."
+                placeholder="Selecteer een aankomststation"
+                zIndex={2000}
+                zIndexInverse={2000}
+                dropDownDirection={"BOTTOM"}
+              />
+            </>
+          )}
+          name="arrival"
         />
       </View>
-      <Text style={title}>Waar naartoe?</Text>
-    </View>
+    </Pressable>
   );
 }
