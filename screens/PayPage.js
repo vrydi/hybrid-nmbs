3e8c42008921d26ce9ad8986c7bc23c26e8d5ee7
vrydi <57484019+vrydi@users.noreply.github.com>
2022-03-27 23:44:50 +0200

    fetch products

--- screens/PayPage.js
+++ screens/PayPage.js
@@ -1,7 +1,20 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+
+import Constants from "expo-constants";
+const { manifest } = Constants;
+const uri = `http://${manifest.debuggerHost.split(":").shift()}:3000`;
 
 export default function PayPage() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      console.log("fetching products");
+      const data = await (await fetch(`${uri}/get-products`)).json();
+      console.log(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <View>
       <Text>PayPage</Text>
--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 import express from "express";
-import { STRIPE_PUBLISHABLE_KEY } from "@env";
+import Stripe from "stripe";
+
+const stripe = Stripe(
+  "sk_test_51KfKzPDyw6addEXMPLzdd4nHIY45F2hrSmVjzOPw5xeo50KXJBsWqaEy5AyCw57UW2Y376hYakc5XxcBFF9DeunG00piKc3s6u"
+);
 
 const app = express();
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
+});
+
+app.get("/get-products", async (req, res) => {
+  try {
+    const products = await stripe.products.list({ limit: 5 });
+    console.log(products);
+    res.json({ products: products });
+  } catch (error) {
+    console.error(error);
+    res.send({ error: error.message });
+  }
 });
