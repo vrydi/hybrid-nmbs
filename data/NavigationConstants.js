e7dfe410ef58624105c7af2a61fc34e6286c4b04
vrydi <57484019+vrydi@users.noreply.github.com>
2022-03-27 22:49:36 +0200

    added pages

--- data/NavigationConstants.js
+++ data/NavigationConstants.js
@@ -1,5 +1,7 @@
 import { HomePage } from "../screens/HomePage";
+import PayPage from "../screens/PayPage";
 import { StationSearchPage } from "../screens/StationSearchPage";
+import TicketPage from "../screens/TicketPage";
 
 export const NAV_HOME = {
   name: "Home",
@@ -14,6 +16,25 @@ export const NAV_STATION_SEARCH = {
   component: StationSearchPage,
 };
 
-export const NAV_ELEMENTS = [NAV_HOME, NAV_STATION_SEARCH];
+export const NAV_TICKETS = {
+  name: "Tickets",
+  icon: "bookmark",
+  color: "white",
+  component: TicketPage,
+};
+
+export const NAV_PAY = {
+  name: "Buy",
+  icon: "payment",
+  color: "white",
+  component: PayPage,
+};
+
+export const NAV_ELEMENTS = [
+  NAV_HOME,
+  NAV_STATION_SEARCH,
+  NAV_PAY,
+  NAV_TICKETS,
+];
 
 export const PAGE_TRAIN_DETAIL = "Trein";
---
+++ screens/TicketPage.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function TicketPage() {
+  return (
+    <View>
+      <Text>TicketPage</Text>
+    </View>
+  );
+}
